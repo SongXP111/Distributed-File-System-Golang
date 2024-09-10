5e2166db076ec8962d5e6a8bb44a6462b61405b1
xipeng <sxp0728@126.com>
2024-09-09 21:53:05 -0400

    created interfaces for transport

---
+++ p2p/transport.go
@@ -0,0 +1,13 @@
+package p2p
+
+// Peer is an interface that represents the remote node
+// Peer代表一个远程节点，通常在对等网络（P2P）中，节点之间会互相通信
+type Peer interface {
+}
+
+// Transport is anything that handles communication
+// between the nodes in the network. This can be the form
+// (TCP, UDP, websockets, ...)
+// Transport 通常用于处理节点之间的通信
+type Transport interface {
+}
